990ae068e0dded50497fe7f97904f89a3a87aa81
AadyaaJhunjhunwala <80698061+AadyaaJhunjhunwala@users.noreply.github.com>
2021-08-10 14:51:54 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -3,6 +3,7 @@ const Engine = Matter.Engine;
 const World = Matter.World;
 const Bodies = Matter.Bodies;
 const Body = Matter.Body;
+const Constraint = Matter.Constraint;
 
 var bob1, bob2, bob3, bob4, bob5
 var roof
